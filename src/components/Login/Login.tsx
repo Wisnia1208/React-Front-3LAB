891299b45acc84485360766e76c6c247194bdb00
Jakub Wisniewski <kwisniew12@gmail.com>
2024-10-28 15:13:15 +0100

    done

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -12,9 +12,9 @@ export default function Login() {
 
   const { authenticated, login } = useContext(AuthContext);
 
-    const handleSubmit = (event: React.FormEvent) => {
-      event.preventDefault();
-      login(userName, userPassword);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    login(userName, userPassword);
   };
 
   useEffect(() => {
@@ -26,35 +26,35 @@ export default function Login() {
 
   return (
     <>
-          <div className="flex flex-col items-center h-full w-full px-80 py-20">
-              <div className="flex flex-col items-center gap-7 w-[400px] h-[400px] rounded-xl bg-slate-300 border-2 border-slate-400">
-                  <p className="mt-10 text-2xl font-semibold tracking-wider uppercase">
-                      Log in
-                  </p>
-                  <form
-                      className="flex flex-col justify-between items-center w-full h-full px-10 pb-10"
-                      onSubmit={(e) => e.preventDefault()}
-                  >
-                      <div className="flex flex-col gap-4">
-                          <Input
-                              label="User login"
-                              value={userName}
-                              onChange={(e) => setUserName(e.target.value)}
-                          />
-                          <Input
-                              label="User password"
-                              value={userPassword}
-                              type="password"
-                              onChange={(e) => setUserPassword(e.target.value)}
-                          />
-                      </div>
-                      <Button text="Sign in" type="submit" onClick={handleSubmit} />
-                      <Link to="/register" className="underline">
-                          Register here
-                      </Link>
-                  </form>
-              </div>
-          </div>
+      <div className="flex flex-col items-center h-full w-full px-80 py-20">
+        <div className="flex flex-col items-center gap-7 w-[400px] h-[400px] rounded-xl bg-slate-300 border-2 border-slate-400">
+          <p className="mt-10 text-2xl font-semibold tracking-wider uppercase">
+            Log in
+          </p>
+          <form
+            className="flex flex-col justify-between items-center w-full h-full px-10 pb-10"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <div className="flex flex-col gap-4">
+              <Input
+                label="User login"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <Input
+                label="User password"
+                value={userPassword}
+                type="password"
+                onChange={(e) => setUserPassword(e.target.value)}
+              />
+            </div>
+            <Button text="Sign in" type="submit" onClick={handleSubmit} />
+            <Link to="/register" className="underline">
+              Register here
+            </Link>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
